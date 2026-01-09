e753961af76c93da9650bd51441fab9aef75e88b
Shelbyen <ars.sheberstov@mail.ru>
2026-01-10 02:55:34 +0300

    feat: user model and migration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=250)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        primary_key=True,
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
